0cff01208bd2719602b20de5fc3608aaf1f8693d
Gustavo Caetano <souljacker@gmail.com>
2021-07-08 17:44:09 -0300

    Add index

---
+++ index.php
@@ -0,0 +1,39 @@
+<h2>Dados enviados via POST</h2>
+<table>
+<?php
+
+
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+
+
+?>
+</table>
+
+<h2>Dados enviados via GET</h2>
+<table>
+<?php
+
+
+    foreach ($_GET as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
+
+
+?>
+</table>
