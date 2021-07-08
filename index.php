72b48078f79a979055f58bcbb07084815ffad24d
Gustavo Caetano <souljacker@gmail.com>
2021-07-08 17:57:49 -0300

    Conditional display and better tables

--- index.php
+++ index.php
@@ -1,39 +1,37 @@
-<h2>Dados enviados via POST</h2>
-<table>
+
 <?php
 
-
+if (!empty($_POST)) {
+    echo "<h2>Dados enviados via POST</h2>";
+    echo "<table>";
+    echo "<thead><tr><td>Chave</td><td>Valor</td></tr></thead>";
     foreach ($_POST as $key => $value) {
         echo "<tr>";
         echo "<td>";
-        echo $key;
+        echo "<strong>$key</strong>";
         echo "</td>";
         echo "<td>";
         echo $value;
         echo "</td>";
         echo "</tr>";
     }
+    echo "</table>";
+}
 
-
-?>
-</table>
-
-<h2>Dados enviados via GET</h2>
-<table>
-<?php
-
-
+if (!empty($_GET)) {
+    echo "<h2>Dados enviados via GET</h2>";
+    echo "<table>";
+    echo "<thead><tr><td>Chave</td><td>Valor</td></tr></thead>";
     foreach ($_GET as $key => $value) {
         echo "<tr>";
         echo "<td>";
-        echo $key;
+        echo "<strong>$key</strong>";
         echo "</td>";
         echo "<td>";
         echo $value;
         echo "</td>";
         echo "</tr>";
     }
-
-
+    echo "</table>";
+}
 ?>
-</table>
